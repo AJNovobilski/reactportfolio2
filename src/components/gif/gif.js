6fdd87093a5e8019e412370111a53b94913d102c
anovobilskischool <anovobilskischool@gmail.com>
2021-02-25 11:42:39 -0800

    image

--- src/components/gif/gif.js
+++ src/components/gif/gif.js
@@ -1,4 +1,5 @@
 import React from "react";
+import holy from "../../assets/homegif2.gif"
 
 function gif() {
 
@@ -6,7 +7,7 @@ function gif() {
         <>
         <div class="grid justify-items-center">
 
-        <img class="mt-5 justify-self-auto" width="300px" height="300px" src="http://ajnportfolio.com/media/homegif2.gif"></img>
+        <img class="mt-5 justify-self-auto" width="300px" height="300px" src={holy}></img>
         </div>
         </>
 
