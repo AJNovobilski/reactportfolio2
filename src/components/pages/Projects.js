90b4686f9ffb84b02ee0baabbefa9aaad1e88ef2
anovobilskischool <anovobilskischool@gmail.com>
2021-02-24 22:22:38 -0800

    project cards

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Gif2 from "../gif/gif2";
+import ProjectCard from "../projectcards/projectcard"
 
 
 export const Projects = () => {
@@ -9,6 +10,82 @@ export const Projects = () => {
       <div>
       <Gif2></Gif2>
     </div>
+
+    <body class="antialiased md:bg-gray-100">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+
+    <ProjectCard
+    src="https://github.com/rschm007/Portfolio_v3/blob/main/src/assets/imgs/Babelbox.png?raw=true"
+    cat="Node Scripts"
+    title="Magic The Gathering Ebay Autolister"
+    description="asodfjaosidfjasoidjf"
+    date="Feb 12th 2021"
+    link="https://github.com/joshuapribek/Project3"
+    >
+    
+    </ProjectCard>
+
+    <ProjectCard
+    src="https://github.com/rschm007/Portfolio_v3/blob/main/src/assets/imgs/Babelbox.png?raw=true"
+    cat="Node Scripts"
+    title="Magic The Gathering Ebay Autolister"
+    description="This unique app is built on the NodeJs framework and uses Inquirer.js to prompt the user for basic info. Axios calls Scryfall api to return live prices for cards, image, set and title data. Then after each card is inputed it outputs a csv file that ebay can read through its bulk listing reader."
+    date="Feb 12th 2021"
+    link="/contact"
+    >
+    
+    </ProjectCard>
+
+    <ProjectCard
+    src="https://github.com/rschm007/Portfolio_v3/blob/main/src/assets/imgs/Babelbox.png?raw=true"
+    cat="Scripts"
+    title="asdfasdf"
+    description="asodfjaosidfjasoidjf"
+    date="Feb 12th 2021"
+    link="/contact"
+    >
+    
+    </ProjectCard>
+
+    <ProjectCard
+    src="https://github.com/rschm007/Portfolio_v3/blob/main/src/assets/imgs/Babelbox.png?raw=true"
+    cat="Scripts"
+    title="asdfasdf"
+    description="asodfjaosidfjasoidjf"
+    date="Feb 12th 2021"
+    link="/contact"
+    >
+    
+    </ProjectCard>
+
+    <ProjectCard
+    src="https://github.com/rschm007/Portfolio_v3/blob/main/src/assets/imgs/Babelbox.png?raw=true"
+    cat="Scripts"
+    title="asdfasdf"
+    description="asodfjaosidfjasoidjf"
+    date="Feb 12th 2021"
+    link="/contact"
+    >
+    
+    </ProjectCard>
+
+    <ProjectCard
+    src="https://github.com/rschm007/Portfolio_v3/blob/main/src/assets/imgs/Babelbox.png?raw=true"
+    cat="Scripts"
+    title="asdfasdf"
+    description="asodfjaosidfjasoidjf"
+    date="Feb 12th 2021"
+    link="/contact"
+    >
+    
+    </ProjectCard>
+
+    
+
+
+    </div>
+</body>
+
     
     </div>
   );
--- src/components/projectcards/projectcard.js
+++ src/components/projectcards/projectcard.js
@@ -1,11 +1,47 @@
 import React from "react";
 
-function ProjectCard() {
+const ProjectCard = (props) => {
+    return(
+
+<div class="md:p-8 p-2 bg-white mt-6">
+        <img
+          class="rounded-lg w-full"
+          src={props.src}
+        />
+
+        <p class="text-indigo-500 font-semibold text-base mt-2">{props.cat}</p>
+        <h1
+          class="font-semibold text-gray-900 leading-none text-xl mt-1 capitalize truncate p-2"
+        >
+          {props.title}
+        </h1>
+        <div class="max-w-full">
+          <p class="text-base font-medium tracking-wide text-gray-600 mt-1">
+           {props.description}
+          </p>
+        </div>
+      <a target="_blank" href={props.link}>  <button class="bg-blue-500 text-2xl hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+  Learn More
+</button>
+</a>
+        <div class="flex items-center space-x-2 mt-10">
+    
+            <p class="text-gray-500 font-semibold text-sm">
+              {props.date}
+               &middot; 
+            </p>
+            <div>
+
+          </div>
+        </div>
+      </div>
 
 
 
 
 
+
+    )
 }
 
 export default ProjectCard
